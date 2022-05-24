495697923e1035c463fa0748bc3a749f87084c22
Elias Oliveira <elias.oliveira@adviseu.local>
2022-05-24 16:34:24 -0300

    resolve dimensions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Card() {
   return (
-    <div className="lg:w-1/3 sm:w-1/2 p-4">
+    <div className="lg:w-1/3 sm:w-1/2 p-1">
       <div className="flex relative">
         <img
           alt="gallery"
           className="absolute inset-0 w-full h-full object-cover object-center"
           src="https://dummyimage.com/600x360"
         />
-        <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+        <div className="px-8 py-6 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
           <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
             THE SUBTITLE
           </h2>
